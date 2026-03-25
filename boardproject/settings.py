c8aa10db2b7523aed615758df501d401d95aa572
watanabe <yuewatanabe.math@gmail.com>
2026-03-25 23:35:10 +0900

    commit 20260325

--- boardproject/settings.py
+++ boardproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'boardproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
