f9ff6723823e4efdf454e62a70561ef846e52e33
kk-watanabe <yuewatanabe.math@gmail.com>
2026-03-25 14:14:07 +0900

    commit 260325

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -3,7 +3,7 @@ from .views import signupfunc, loginfunc, listfunc, logoutfunc, detailfunc, good
 
 urlpatterns = [
     path('signup/', signupfunc, name='signup'),
-    path('login/', loginfunc, name='login'),
+    path('', loginfunc, name='login'),
     path('list/', listfunc, name='list'),
     path('logout/', logoutfunc, name='logout'),
     path('detail/<int:pk>', detailfunc, name='detail'),
